5d0b5d18bfd02459cb79975ea7612927c9d0c844
Steve Rivera <srivera.x1@gmail.com>
2018-12-01 19:40:59 -0500

    set up placeholders for auto-updater events

--- main-process/window-handlers/splash.js
+++ main-process/window-handlers/splash.js
@@ -1,12 +1,13 @@
 // @flow
 import path from 'path';
 import is from 'electron-is';
+import { autoUpdater } from 'electron-updater';
 import WindowManager from 'main/lib/window-manager';
 
 
 // module-level variables and constants
 const PORT = process.env.PORT || 3000;
-const WIDTH = 200;
+const WIDTH = 300;
 const HEIGHT = 400;
 const CONFIG = {
   url: is.production()
@@ -21,6 +22,28 @@ const CONFIG = {
   }
 };
 
+
+// auto updater event handlers
+function handleCheckingUpdate() {
+  // @TODO
+}
+
+function handleDownloadProgress( progressObj: Object ) {
+  // @TODO
+}
+
+function handleUpdateDownloaded( info: Object ) {
+  // @TODO
+}
+
+
 export default () => {
+  // auto updater logic and events
+  autoUpdater.checkForUpdates();
+  autoUpdater.on( 'checking-for-update', handleCheckingUpdate );
+  autoUpdater.on( 'download-progress', handleDownloadProgress );
+  autoUpdater.on( 'update-downloaded', handleUpdateDownloaded );
+
+  // create the window
   WindowManager.createWindow( '/windows/splash', CONFIG.url, CONFIG.opts );
 };
